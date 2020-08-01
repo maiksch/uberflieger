ff39ff68f830a9dd435e4df77b6d9b5ce314e25c
Maik Schmaddebeck <maik.schmaddebeck@point-gmbh.com>
2020-08-01 21:43:23 +0200

    (client): Added lesson route

---
+++ client/src/routes/lessons/[slug].svelte
@@ -0,0 +1,41 @@
+<script context="module">
+	export async function preload({ params, query }) {
+		const res = await this.fetch(`http://localhost:3000/lessons/1`);
+		const lesson = await res.json();
+		return { lesson };
+	}
+</script>
+
+<script>
+	export let lesson;
+</script>
+
+<style>
+	h1 {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Uberflieger - {lesson.title}</title>
+</svelte:head>
+
+<h1>{lesson.title}</h1>
+
+<video crossorigin="anonymous" preload="auto" controls>
+	<source src="small.webm" type="video/webm" />
+</video>
